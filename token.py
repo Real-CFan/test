e210fb1e63238672044f7e45c2a8eb65bcae40d0
Real-CFan <zhangfcw@163.com>
2022-03-03 20:43:38 +0800

    Initial commit

---
+++ token.py
@@ -0,0 +1,6 @@
+
+print('Using password can not login my github account in pycharm, so I have to use the token!')
+
+print('the token is : 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')
+
+#  https://blog.csdn.net/qq_43060870/article/details/119409720
